e6d96f501002de92662647391f799f89d4d05567
Joshua-Patton <105231202+Joshua-Patton@users.noreply.github.com>
2024-10-16 16:49:38 +0800

    bug fixes

--- simulation.c
+++ simulation.c
@@ -1,3 +1,5 @@
+//              Studentid   NAME
+// student1     23103979    JOSHUA PATTON
 
 //      IMPORTS
 #include <stdio.h>
@@ -16,6 +18,14 @@
 //      GLOBAL
 int timestep = 0;
 
+// initialize page tables, allocate to disc
+int page_tables[PROCESS_COUNT][PAGE_COUNT] = {
+    {ON_DISC, ON_DISC, ON_DISC, ON_DISC},
+    {ON_DISC, ON_DISC, ON_DISC, ON_DISC},
+    {ON_DISC, ON_DISC, ON_DISC, ON_DISC},
+    {ON_DISC, ON_DISC, ON_DISC, ON_DISC}
+};
+
 //      STRUCTURES
 struct mem {
     int process_id;
@@ -26,16 +36,7 @@ struct mem {
 struct mem *ram[RAM_SIZE];
 struct mem *vmem[VMEMSIZE];
 
-
 //      FUNCTIONS
-// initialize page tables, allocate to disc
-int page_tables[PROCESS_COUNT][PAGE_COUNT] = {
-    {ON_DISC, ON_DISC, ON_DISC, ON_DISC},
-    {ON_DISC, ON_DISC, ON_DISC, ON_DISC},
-    {ON_DISC, ON_DISC, ON_DISC, ON_DISC},
-    {ON_DISC, ON_DISC, ON_DISC, ON_DISC}
-};
-
 // ram init function
 void init_ram(){
     for (int i = 0; i < RAM_SIZE; i++) {
@@ -43,15 +44,14 @@ void init_ram(){
     }
 }
 
-// vmem init func
+// vmem init function
 void init_vmem(){
-    for (int process_id = 0;process_id<PROCESS_COUNT;process_id++){
-        for (int page_num = 0;page_num<PAGE_COUNT;page_num++){
-
+    for (int process_id = 0; process_id < PROCESS_COUNT; process_id++) {
+        for (int page_num = 0; page_num < PAGE_COUNT; page_num++) {
             struct mem *page = malloc(sizeof(struct mem));  // Create a new page
             page->process_id = process_id;      // Set the process ID
             page->page_num = page_num;        // Set the page number
-            page->last_accessed = 0;   // Initialize last accessed time to 0
+            page->last_accessed = 0;          // Initialize last accessed time to 0
 
             vmem[(process_id * 8) + (page_num * 2)] = page;
             vmem[(process_id * 8) + (page_num * 2) + 1] = page;
@@ -59,60 +59,120 @@ void init_vmem(){
     }
 }
 
-// request page and take it to RAM func
-// will need LRU function as helper
-void page_request(int process_id, int page_num, int timestep){
+// LRU eviction function
+int evict_LRU() {
+    int oldest_time = INT_MAX;
+    int oldest_frame = -1;
+
+    for (int i = 0; i < RAM_SIZE; i++) {
+        if (ram[i] != NULL && ram[i]->last_accessed < oldest_time) {
+            oldest_frame = i;
+            oldest_time = ram[i]->last_accessed;
+        }
+    }
+
+    return oldest_frame;  // Return the frame number of the least recently used page
+}
+
+// Load page into RAM
+void load_page_to_ram(int process_id, int page_num, int frame) {
+    ram[frame] = vmem[(process_id * PAGE_COUNT * 2) + (page_num * 2)];  // Copy from virtual memory to RAM
+    ram[frame]->last_accessed = timestep;  // Update last accessed time
+    page_tables[process_id][page_num] = frame;  // Update page table
+}
+
+// Request page and bring it to RAM
+void page_request(int process_id, int page_num) {
     int frame_num = page_tables[process_id][page_num];
-    if (frame_num != 99) {
+
+    if (frame_num != ON_DISC) {
+        // Page is already in RAM
         ram[frame_num]->last_accessed = timestep;  // Update last accessed time
-        (timestep)++;  // Increment global time
+        timestep++;  // Increment global time
         return;
+    } else {
+        // Page is not in RAM, find space or evict a page
+        int empty_frame = -1;
+        for (int i = 0; i < RAM_SIZE; i++) {
+            if (ram[i] == NULL) {
+                empty_frame = i;
+                break;
+            }
+        }
+
+        if (empty_frame != -1) {
+            // We found an empty frame, load the page there
+            load_page_to_ram(process_id, page_num, empty_frame);
+        } else {
+            // No empty frame, evict a page using LRU
+            int evicted_frame = evict_LRU();
+            load_page_to_ram(process_id, page_num, evicted_frame);
+        }
+        timestep++;  // Increment global time after loading a new page
     }
 }
 
-// LRU func using local and global
-// returns an int so we know which frame we are going to overwrite
-int LRU(int process_id){
-}
-
-
-// simulation func
-// uses page_request
-void simulate(char *input_file){
+// Simulation function (uses page_request)
+void simulate(char *input_file) {
     // Open input file
     FILE* infile = fopen(input_file, "r");
     if (infile == NULL) {
         perror("Error opening input file");
-        return 1;
+        return;
     }
 
+    int process_id, page_num;
+    while (fscanf(infile, "%d %d", &process_id, &page_num) != EOF) {
+        page_request(process_id, page_num);
+    }
 
+    fclose(infile);
 }
 
-// output print func
-void print_results(char *output_file){
-    FILE* outfile = fopen(output_file, "r");
+// Output print function
+void print_results(char *output_file) {
+    FILE* outfile = fopen(output_file, "w");
     if (outfile == NULL) {
-        perror("Error opening input file");
-        return 1;
+        perror("Error opening output file");
+        return;
     }
 
+    // Print page tables
+    for (int process_id = 0; process_id < PROCESS_COUNT; process_id++) {
+        for (int page_num = 0; page_num < PAGE_COUNT; page_num++) {
+            fprintf(outfile, "%d ", page_tables[process_id][page_num]);
+        }
+        fprintf(outfile, "\n");
+    }
 
+    // Print RAM state
+    for (int i = 0; i < RAM_SIZE; i++) {
+        if (ram[i] != NULL) {
+            fprintf(outfile, "%d,%d,%d; ", ram[i]->process_id, ram[i]->page_num, ram[i]->last_accessed);
+        } else {
+            fprintf(outfile, "empty; ");
+        }
+    }
+    fprintf(outfile, "\n");
+
+    fclose(outfile);
 }
 
-int main(int argc, char *argv[]){
-    if (argc != 3){
+// Main function
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
         printf("Usage: %s <input_file> <output_file>\n", argv[0]);
         return 1;
     }
 
-    // initialise memory 
+    // Initialize memory
     init_ram();
-    init_vmem(); 
+    init_vmem();
 
-    int time = 0;
+    // Simulate the memory management
     simulate(argv[1]);
 
+    // Print the results
     print_results(argv[2]);
 
     return 0;
